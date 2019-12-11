fffe343d5222792dc014e5d11aa0496f01f0f724
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-10 18:53:31 -0800

    defined the routes in server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ connectDB(); // connects database
 
 app.get("/", (req, res) => res.send("API Running")); // single endpoint to test
 
+// defines the routes / allows access to the routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000; // looks for an environment variable called Port to use when deployed to Heroku || locally runs on Port 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
