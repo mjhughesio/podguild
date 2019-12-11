90176cd04b86bd8c2bb9207716b220f4986c72c3
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-10 19:24:40 -0800

    created the User model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // prevents users from registering with the same email
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now, // sets at current date and time
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
