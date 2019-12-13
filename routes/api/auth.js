b2cf68a16ea346389d4e3769268034cf47931b77
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-12 20:14:32 -0800

    updated auth api to include verification and return user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,21 @@
 
 const express = require("express"); // brings in express
 const router = express.Router();
+const auth = require("../../middleware/auth"); // protects routes by adding as a param
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Auth route")); // creates a route
+router.get("/", auth, async (req, res) => {
+  // creates a route
+  try {
+    const user = await User.findById(req.user.id).select("-password"); // last bit leaves off password in the data
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
