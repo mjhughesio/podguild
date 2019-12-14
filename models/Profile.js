dfe8e25bc5fc9201d8db210b8eab02a007427dd3
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-13 19:13:34 -0800

    created profile model

---
+++ models/Profile.js
@@ -0,0 +1,117 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    // e.g. city, state, country
+    type: String,
+  },
+  status: {
+    // i.e. level of expertise
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String], // comma separated value list - will use JS to convert into array and put in database
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean, // will feature a checkbox
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
+// when you call mongoose.model() on a schema, Mongoose compiles a model for you.
+// the first argument is the singular name of the collection your model is for.
