daf408546c3635cab194727f2d1c64535bd17282
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-16 21:09:30 -0800

    set up route to delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -148,4 +148,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // removes profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // removes user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
