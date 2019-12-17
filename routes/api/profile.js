5a694ae4e147f0c72eefc5fd859bb06afeb61680
Matthew J. Hughes <mjhughes707@gmail.com>
2019-12-16 19:42:26 -0800

    added post route to create or update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,11 @@
 const express = require("express"); // brings in express
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+// requires to express-validator/check are deprecated, so just using express-validator instead
+// documentation for express-validator/check - https://express-validator.github.io/docs/
+const mongoose = require("mongoose"); // brought in to resolve deprecation warning
+
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
@@ -25,5 +30,93 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 }); // creates a route
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // if there are errors
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      // pulls everything out from the body
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    const profileFields = {}; // builds profile fields object
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      // trims whitespace and converts skills into an array
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    profileFields.social = {}; // builds social object
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id }); // finds profile by user
+
+      mongoose.set("useFindAndModify", false);
+      // added due to following: DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
+      // more info at https://mongoosejs.com/docs/deprecations.html#-findandmodify-
+
+      if (profile) {
+        // if profile found, updates
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields); // if profile not found, creates
+
+      await profile.save(); // saves profile
+      res.json(profile); // sends back profile
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
