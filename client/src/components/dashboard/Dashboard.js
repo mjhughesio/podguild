35425f98943cb0d7253af326b8e5000bd855a52b
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-04 20:51:23 -0800

    added components to add education and experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -19,67 +19,67 @@ const Dashboard = ({
     <Spinner />
   ) : (
     <Fragment>
-      <h1 class="large text-primary">Dashboard</h1>
-      <p class="lead">
-        <i class="fas fa-user"></i> Welcome, {user && user.name}!
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome, {user && user.name}!
       </p>
       {profile !== null ? (
         <Fragment>
           {" "}
           <DashboardActions />
-          <h2 class="my-2">Experience Credentials</h2>
-          <table class="table">
+          <h2 className="my-2">Experience Credentials</h2>
+          <table className="table">
             <thead>
               <tr>
                 <th>Company</th>
-                <th class="hide-sm">Title</th>
-                <th class="hide-sm">Years</th>
+                <th className="hide-sm">Title</th>
+                <th className="hide-sm">Years</th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>Microsoft</td>
-                <td class="hide-sm">Senior Developer</td>
-                <td class="hide-sm">Oct 2011 - Current</td>
+                <td className="hide-sm">Senior Developer</td>
+                <td className="hide-sm">Oct 2011 - Current</td>
                 <td>
-                  <button class="btn btn-danger">Delete</button>
+                  <button className="btn btn-danger">Delete</button>
                 </td>
               </tr>
               <tr>
                 <td>Sun Microsystems</td>
-                <td class="hide-sm">Senior Developer</td>
-                <td class="hide-sm">Oct 2004 - Nov 2010</td>
+                <td className="hide-sm">Senior Developer</td>
+                <td className="hide-sm">Oct 2004 - Nov 2010</td>
                 <td>
-                  <button class="btn btn-danger">Delete</button>
+                  <button className="btn btn-danger">Delete</button>
                 </td>
               </tr>
             </tbody>
           </table>
-          <h2 class="my-2">Education Credentials</h2>
-          <table class="table">
+          <h2 className="my-2">Education Credentials</h2>
+          <table className="table">
             <thead>
               <tr>
                 <th>School</th>
-                <th class="hide-sm">Degree</th>
-                <th class="hide-sm">Years</th>
+                <th className="hide-sm">Degree</th>
+                <th className="hide-sm">Years</th>
                 <th></th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>University of Washington</td>
-                <td class="hide-sm">Masters</td>
-                <td class="hide-sm">Sep 1993 - June 1999</td>
+                <td className="hide-sm">Masters</td>
+                <td className="hide-sm">Sep 1993 - June 1999</td>
                 <td>
-                  <button class="btn btn-danger">Delete</button>
+                  <button className="btn btn-danger">Delete</button>
                 </td>
               </tr>
             </tbody>
           </table>
-          <div class="my-2">
-            <button class="btn btn-danger">
-              <i class="fas fa-user-minus"></i> Delete My Account
+          <div className="my-2">
+            <button className="btn btn-danger">
+              <i className="fas fa-user-minus"></i> Delete My Account
             </button>
           </div>
         </Fragment>
