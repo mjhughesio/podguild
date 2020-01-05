54d6233f6982e8622dbd14b46af05d5cb8c6912e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-04 17:28:07 -0800

    started setting up the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,110 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
 import { getCurrentProfile } from "../../actions/profile";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 class="large text-primary">Dashboard</h1>
+      <p class="lead">
+        <i class="fas fa-user"></i> Welcome, {user && user.name}!
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          {" "}
+          <div class="dash-buttons">
+            <a href="create-profile.html" class="btn">
+              <i class="fas fa-user-circle text-primary"></i> Edit Profile
+            </a>
+            <a href="add-experience.html" class="btn">
+              <i class="fab fa-black-tie text-primary"></i> Add Experience
+            </a>
+            <a href="add-education.html" class="btn">
+              <i class="fas fa-graduation-cap text-primary"></i> Add Education
+            </a>
+          </div>
+          <h2 class="my-2">Experience Credentials</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Company</th>
+                <th class="hide-sm">Title</th>
+                <th class="hide-sm">Years</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Microsoft</td>
+                <td class="hide-sm">Senior Developer</td>
+                <td class="hide-sm">Oct 2011 - Current</td>
+                <td>
+                  <button class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+              <tr>
+                <td>Sun Microsystems</td>
+                <td class="hide-sm">Senior Developer</td>
+                <td class="hide-sm">Oct 2004 - Nov 2010</td>
+                <td>
+                  <button class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <h2 class="my-2">Education Credentials</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>School</th>
+                <th class="hide-sm">Degree</th>
+                <th class="hide-sm">Years</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>University of Washington</td>
+                <td class="hide-sm">Masters</td>
+                <td class="hide-sm">Sep 1993 - June 1999</td>
+                <td>
+                  <button class="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="my-2">
+            <button class="btn btn-danger">
+              <i class="fas fa-user-minus"></i> Delete My Account
+            </button>
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>
+            You have not yet set up a profile. To do so, please click the button
+            below.
+          </p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
