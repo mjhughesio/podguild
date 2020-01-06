5644a8f6d49a04e9c8009ae4a60ee8876ee3f57e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-05 18:37:15 -0800

    added ability to display profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -62,7 +62,7 @@ export const getProfileById = userId => async dispatch => {
   }
 };
 
-// Get Gitub repos
+// Get Github repos
 export const getGithubRepos = username => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
