d50759593fecc7c0af58afb181a74b7f6049850c
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-10 15:46:33 -0800

    added ability to delete user posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -36,6 +36,22 @@ export const removeLike = postId => async dispatch => {
     const res = await axios.put(`/api/posts/unlike/${postId}`);
 
     dispatch({ type: UPDATE_LIKES, payload: { id: postId, likes: res.data } });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = postId => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`);
+
+    dispatch({ type: DELETE_POST, payload: postId });
+
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
