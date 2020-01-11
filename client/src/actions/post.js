93f2124820142f3d60603da82b59d95b306d7f35
Matthew J. Hughes <mjhughes707@gmail.com>
2020-01-10 16:29:58 -0800

    added ability to display single posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
@@ -14,6 +15,20 @@ export const getPosts = () => async dispatch => {
     const res = await axios.get("/api/posts");
 
     dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get post
+export const getPost = postId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${postId}`);
+
+    dispatch({ type: GET_POST, payload: res.data });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
